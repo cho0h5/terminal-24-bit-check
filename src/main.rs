7c07ea8727cef3acf88af222fbbce0049a184b0e
Youngho Cho <dominoyh5@gmail.com>
2022-07-23 13:52:22 +0000

    formatting

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ use std::cmp;
 fn main() {
     for i in (1..255).step_by(10) {
         for j in (1..255).step_by(10) {
-            print!("\x1b[38;2;{};{};{}mWW\x1b[0m ", cmp::max(0, 250 - i - j), i, j);
+            print!(
+                "\x1b[38;2;{};{};{}mWW\x1b[0m ",
+                cmp::max(0, 250 - i - j),
+                i,
+                j
+            );
         }
         println!();
     }
